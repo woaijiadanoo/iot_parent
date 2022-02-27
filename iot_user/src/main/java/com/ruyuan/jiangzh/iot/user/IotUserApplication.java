f864d4c23d319b098ab95456d5d7d4713fed8b55
jiangzheng <jz292679@sina.com>
2022-02-27 10:49:36 +0800

    用户模块包依赖导入及基础配置演示

---
+++ iot_user/src/main/java/com/ruyuan/jiangzh/iot/user/IotUserApplication.java
@@ -0,0 +1,13 @@
+package com.ruyuan.jiangzh.iot.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotUserApplication.class, args);
+    }
+
+}
