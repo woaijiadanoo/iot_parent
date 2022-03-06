63e97af45a467af913f073a6cfe9fe1b914ec814
jiangzheng <jz292679@sina.com>
2022-03-06 22:21:25 +0800

    工具相关内容封装

--- iot_user/src/main/java/com/ruyuan/jiangzh/iot/user/controller/UserController.java
+++ iot_user/src/main/java/com/ruyuan/jiangzh/iot/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ruyuan.jiangzh.iot.user.controller;
 
+import com.ruyuan.jiangzh.iot.base.exception.AppException;
 import com.ruyuan.jiangzh.iot.base.web.RespDTO;
 import com.ruyuan.jiangzh.iot.user.dao.entity.IotUser;
 import com.ruyuan.jiangzh.iot.user.service.UserService;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 @RestController
@@ -25,6 +27,13 @@ public class UserController {
     @RequestMapping(value = "/getall",method = RequestMethod.GET)
     public RespDTO getUser(){
         List<IotUser> iotUsers = userService.showXml();
+
+        // 假装有传入一个username
+        String msgId = "username_is_null";
+        String username = "";
+        if(username.trim().length() == 0){
+//            throw new AppException(msgId);
+        }
 
         return RespDTO.success(iotUsers);
     }
