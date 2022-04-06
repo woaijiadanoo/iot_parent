dca63c79e6362275dcbce5fbf94bc54a5a092c4e
jiangzheng <jz292679@sina.com>
2022-04-06 22:36:15 +0800

    提交代码

---
+++ iot_ddd_demo/src/main/java/com/ruyuan/jiangzh/ddd/demo/domain/aggregates/aggregateXXX/infrastructure/repository/XXXRepository.java
@@ -0,0 +1,7 @@
+package com.ruyuan.jiangzh.ddd.demo.domain.aggregates.aggregateXXX.infrastructure.repository;
+
+public interface XXXRepository {
+
+    boolean save();
+
+}
---
+++ iot_ddd_demo/src/main/java/com/ruyuan/jiangzh/ddd/demo/domain/infrastructure/repository/XXXRepository.java
@@ -0,0 +1,7 @@
+package com.ruyuan.jiangzh.ddd.demo.domain.infrastructure.repository;
+
+public interface XXXRepository {
+
+    boolean save();
+
+}
