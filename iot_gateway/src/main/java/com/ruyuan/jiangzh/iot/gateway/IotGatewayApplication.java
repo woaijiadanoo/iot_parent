26aad78f350fa680c81349979bd6aaa7077795b2
jiangzheng <jz292679@sina.com>
2022-04-14 23:03:54 +0800

    提交代码

--- iot_gateway/src/main/java/com/ruyuan/jiangzh/iot/gateway/IotGatewayApplication.java
+++ iot_gateway/src/main/java/com/ruyuan/jiangzh/iot/gateway/IotGatewayApplication.java
@@ -2,8 +2,10 @@ package com.ruyuan.jiangzh.iot.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
+@ComponentScan(basePackages = {"com.ruyuan.jiangzh.iot"})
 @SpringBootApplication
 public class IotGatewayApplication {
 
