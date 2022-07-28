9457c7657378b726a0a1cb2c80a4932a68723b1a
jiangzheng <jz292679@sina.com>
2022-07-28 23:02:01 +0800

    提交代码

--- iot_rule_engine/src/main/java/com/ruyuan/jiangzh/iot/rule/domain/infrastructure/utils/ConsistContext.java
+++ iot_rule_engine/src/main/java/com/ruyuan/jiangzh/iot/rule/domain/infrastructure/utils/ConsistContext.java
@@ -3,8 +3,11 @@ package com.ruyuan.jiangzh.iot.rule.domain.infrastructure.utils;
 public interface ConsistContext {
 
     public static final String RULE_CHAIN_QUERY_PARAM_NAME="ruleChainName";
+    public static final String TENANT_ID="tenantId";
 
     // msgId
     public static final String RULE_CHAIN_NAME_EXISTS="rule.rule_chain_name_exists";
+    public static final String RULE_CHAIN_ID_EMPTY="rule.rule_chain_id_empty";
+    public static final String RESOURCE_NOT_EXISTS="default.resource_not_exists";
 
 }
